8ad7457e6a3e0097c7ae9c490d61b19d6ce541fa
Yeison Perea <yeisonestivenperea@gmail.com>
2023-09-08 09:11:14 -0500

    First commit

---
+++ helloWorld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ruta al archivo JAR (ajusta la ruta y el nombre según tu archivo JAR)
+ruta_al_jar=/usr/local/src/HelloWorldJava.jar
+
+# Comando para ejecutar el programa Java
+comando_java="java -jar $ruta_al_jar"
+
+# Ejecuta el programa Java
+$comando_java
---
+++ src/main/java/com/projects/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.projects.helloworld;
+
+/**
+ *
+ * @author Yeison Perea
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
